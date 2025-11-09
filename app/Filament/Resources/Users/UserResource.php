b7e8aabfdec4a6ec9b2b02131ba021ec4f5badf5
zchvm2 <uhrinjozef@gmail.com>
2025-11-09 20:56:11 +0100

    queen4

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -15,6 +15,7 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
 
 class UserResource extends Resource
 {
@@ -33,6 +34,40 @@ class UserResource extends Resource
     {
         return UsersTable::configure($table);
     }
+    // KĽÚČOVÁ ZMENA: Filtrovanie záznamov pre zoznam
+    // Táto metóda definuje, aké záznamy sa vôbec zobrazia v zozname.
+    public static function getEloquentQuery(): Builder
+    {
+        // 1. Získanie základného dopytu
+        $query = parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+
+        // 2. Získanie mena prihláseného používateľa (admina)
+        // Používame globálnu funkciu \auth() pre spoľahlivé získanie prihláseného používateľa.
+        // Pridávame kontrolu, či je používateľ prihlásený.
+        if (Auth::check()) {
+            // Predpokladáme, že meno je v stĺpci 'name' modelu User
+            //$adminName = Auth::user()->name;
+            $skratka_chovu = Auth::user()->skratka_chovu;
+            $opravnenie = Auth::user()->opravnenie;
+            if ($opravnenie == 9) {
+                // Ak je oprávnenie 9, vrátime všetky záznamy bez filtrovania
+                return $query->orderBy('priezvisko', 'asc');
+            } else {
+                // Pokračujeme na filtrovanie nižšie
+
+
+                // 3. Aplikovanie podmienky filtrovania
+                // Filtrujeme, aby 'skratka_chovu' bola rovná menu prihláseného užívateľa
+                return $query->where('skratka_chovu', $skratka_chovu)->orderBy('priezvisko', 'asc');
+            }
+        }
+
+        // Ak nie je prihlásený, nezobrazujeme žiadne záznamy (alebo sa Filament postará o redirect)
+        return $query->whereRaw('1 = 0');
+    }
 
     public static function getRelations(): array
     {
@@ -50,6 +85,7 @@ class UserResource extends Resource
         ];
     }
 
+
     public static function getRecordRouteBindingEloquentQuery(): Builder
     {
         return parent::getRecordRouteBindingEloquentQuery()
@@ -57,8 +93,8 @@ class UserResource extends Resource
                 SoftDeletingScope::class,
             ]);
     }
-            // preklad názvov tabuliek
-        public static function getModelLabel(): string
+    // preklad názvov tabuliek
+    public static function getModelLabel(): string
     {
         return 'Chovateľa matiek';
     }
@@ -72,5 +108,4 @@ class UserResource extends Resource
     {
         return 'Chovatelia matiek';
     }
-
 }
